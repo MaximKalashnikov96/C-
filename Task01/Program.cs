98003a467186b5f5999424975ba0e11e816ad72e
Maxim Kalashnikov <kvozemoda@mail.ru>
2022-12-04 22:37:26 +0400

    Task01+

--- Task01/Program.cs
+++ Task01/Program.cs
@@ -6,3 +6,10 @@
 // a = 9, b = -3 -> да
 // a = -3 b = 9 -> нет
 
+Console.WriteLine("Введите первое число: ");
+int number1 = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите второе число: ");
+int number2 = Convert.ToInt32(Console.ReadLine());
+int square = number1 / number2;
+if(square == number2) Console.WriteLine("Первое число является квадратом второго");
+else Console.WriteLine("Первое не число является квадратом второго");
