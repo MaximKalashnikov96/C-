76b0bc5f5dee7cd6096b63da7d734d62b835ebe1
Maxim Kalashnikov <kvozemoda@mail.ru>
2022-12-20 16:39:57 +0400

    task09+

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -5,3 +5,11 @@
 // 12-> 2
 // 85 -> 8
 
+int number = new Random().Next(10, 100);
+Console.WriteLine($"Случайное число из отрезка 10-99 --> {number}"); 
+
+int firstDigit = number / 10;
+int secondDigit = number % 10; 
+
+if (firstDigit > secondDigit) Console.WriteLine($"Наибольная цифра числа --> {firstDigit}");
+else Console.WriteLine($"Наибольшая цифра числа --> {secondDigit}");
