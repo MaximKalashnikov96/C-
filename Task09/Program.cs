fa706527540475f9e4bdf93d0a35fb4d18ac151b
Maxim Kalashnikov <kvozemoda@mail.ru>
2022-12-20 17:46:11 +0400

    task11+

--- Task09/Program.cs
+++ Task09/Program.cs
@@ -6,10 +6,24 @@
 // 85 -> 8
 
 int number = new Random().Next(10, 100);
-Console.WriteLine($"Случайное число из отрезка 10-99 --> {number}"); 
+Console.WriteLine($"Случайное число из отрезка 10-99 --> {number}");
 
-int firstDigit = number / 10;
-int secondDigit = number % 10; 
+// int firstDigit = number / 10;
+// int secondDigit = number % 10; 
 
-if (firstDigit > secondDigit) Console.WriteLine($"Наибольная цифра числа --> {firstDigit}");
-else Console.WriteLine($"Наибольшая цифра числа --> {secondDigit}");
+// if (firstDigit > secondDigit) Console.WriteLine($"Наибольная цифра числа --> {firstDigit}");
+// else Console.WriteLine($"Наибольшая цифра числа --> {secondDigit}");
+
+int MaxDigit(int num) // int num = number
+{
+    int firstDigit = num / 10; // 7
+    int secondDigit = num % 10; // 8
+                                // if (firstDigit > secondDigit) return firstDigit;
+                                // return secondDigit;
+
+    // int max = firstDigit > secondDigit ? firstDigit : secondDigit;
+    return firstDigit > secondDigit ? firstDigit : secondDigit;
+}
+
+int maxDigit = MaxDigit(number);
+Console.WriteLine($"Наибольшая цифра числа --> {maxDigit}");
---
+++ Task11/Program.cs
@@ -0,0 +1,33 @@
+﻿// Напишите программу, которая выводит случайное
+// трёхзначное число и удаляет вторую цифру этого
+// числа.
+// 456 -> 46
+// 782 -> 72
+// 918 -> 98
+
+int number = new Random().Next(100,1000);
+Console.WriteLine($"случайное трехзначное число: {number}");
+
+int firstDigit = number / 100;
+int secondDigit = number / 10 % 10;
+int thirdDigit = number % 10;
+
+Console.WriteLine(firstDigit);
+Console.WriteLine(secondDigit);
+Console.WriteLine(thirdDigit);
+
+int doubleNumber = firstDigit * 10 + thirdDigit;
+Console.WriteLine(doubleNumber);
+
+
+// int number = new Random().Next(100,1000);
+// Console.WriteLine(number);
+
+// int RemoveSecondDigit(int numb)
+// {
+//     int firstDigit = numb / 100;
+//     int secondDigit = numb % 10;
+//     int result = firstDigit * 10 + secondDigit; 
+//     return result;
+// }
+// Console.WriteLine(RemoveSecondDigit(number));
