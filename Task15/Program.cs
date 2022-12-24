cf69f440a03d13800cc11b230971128e9f4c548b
Maxim Kalashnikov <kvozemoda@mail.ru>
2022-12-24 20:53:43 +0400

    Task15+

--- Task15/Program.cs
+++ Task15/Program.cs
@@ -5,3 +5,19 @@
 // 6 -> да
 // 7 -> да
 // 1 -> нет
+
+Console.WriteLine("Введите номер дня: ");
+int number = Convert.ToInt32(Console.ReadLine());
+// if(number == 1) Console.WriteLine("этот день рабочий");
+// else if(number == 2) Console.WriteLine("этот день рабочий");
+// else if(number == 3) Console.WriteLine("этот день рабочий");
+// else if(number == 4) Console.WriteLine("этот день рабочий");
+// else if(number == 5) Console.WriteLine("этот день рабочий");
+// else if(number == 6) Console.WriteLine("этот день выходной");
+// else if(number == 7) Console.WriteLine("этот день выходной");
+// else Console.WriteLine("Такого дня нет");
+
+if (number == 6 || number == 7)
+Console.WriteLine("да");
+else 
+Console.WriteLine("нет");
