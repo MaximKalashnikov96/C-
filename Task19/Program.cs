7f7b6dd0960c465f5330e50fbbae512fe2516aba
Maxim Kalashnikov <kvozemoda@mail.ru>
2023-02-03 15:39:40 +0400

    Task19+++

--- Task19/Program.cs
+++ Task19/Program.cs
@@ -10,22 +10,23 @@
 
 Console.WriteLine("Введите число: ");
 int number = Convert.ToInt32(Console.ReadLine());
- string s =Console.ReadLine();
+
 
 void Palindrome(int num)
 {
 
-    while (num > 0)
-    {
-        int Temp = num;
-        int rev = 0;
-        int dig = num % 10;
-        rev = rev * 10 + dig;
-        num = num / 10;
-    
-        if (Temp == rev) Console.WriteLine("Это палиндром");
-        else Console.WriteLine("Это не палиндром");
-    }
+
+    int num1 = 0;
+    int a1 = num % 10;
+    int a2 = num / 10 % 10;
+    int a3 = num / 100 % 10;
+    int a4 = num / 1000 % 10;
+    int a5 = num / 10000;
+    num1 = a1 * 10000 + a2 * 1000 + a3 * 100 + a4 * 10 + a5;
+
+    if (num == num1) Console.WriteLine("Это палиндром");
+    else Console.WriteLine("Это не палиндром");
+
 }
 
 Palindrome(number);
